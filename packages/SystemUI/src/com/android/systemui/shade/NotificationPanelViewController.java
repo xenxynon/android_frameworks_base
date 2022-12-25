7a6d6721a01a9c245bfde9cc54687c660a5b930b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-26 03:22:20 +0530

    SystemUI: Combined QS header fixes
    Fixes:
    - Too low margin above QQS
    - Missing signal icons in QQS when >1 SIM
    - Privacy chip not clickable in QS
    - Too low padding between clock and date in QQS
    
    Change-Id: Ice4ddb8fab4542d386e65818c58172d1d30c03ef

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -430,6 +430,7 @@ public final class NotificationPanelViewController extends PanelViewController {
     private int mDisplayRightInset = 0; // in pixels
     private int mLargeScreenShadeHeaderHeight;
     private int mSplitShadeNotificationsScrimMarginBottom;
+    private int mQsTopMargin;
 
     private final KeyguardClockPositionAlgorithm
             mClockPositionAlgorithm =
@@ -652,6 +653,7 @@ public final class NotificationPanelViewController extends PanelViewController {
     private int mScreenCornerRadius;
     private boolean mQSAnimatingHiddenFromCollapsed;
     private boolean mUseLargeScreenShadeHeader;
+    private boolean mUseCombinedQSHeaders;
 
     private int mQsClipTop;
     private int mQsClipBottom;
@@ -1164,6 +1166,7 @@ public final class NotificationPanelViewController extends PanelViewController {
 
         mUseLargeScreenShadeHeader =
                 LargeScreenUtils.shouldUseLargeScreenShadeHeader(mView.getResources());
+        mUseCombinedQSHeaders = mFeatureFlags.isEnabled(Flags.COMBINED_QS_HEADERS);
 
         mLargeScreenShadeHeaderHeight =
                 mResources.getDimensionPixelSize(R.dimen.large_screen_shade_header_height);
@@ -1171,6 +1174,9 @@ public final class NotificationPanelViewController extends PanelViewController {
                 SystemBarUtils.getQuickQsOffsetHeight(mView.getContext());
         int topMargin = mUseLargeScreenShadeHeader ? mLargeScreenShadeHeaderHeight :
                 mResources.getDimensionPixelSize(R.dimen.notification_panel_margin_top);
+        mQsTopMargin = (mUseLargeScreenShadeHeader || !mUseCombinedQSHeaders) ? topMargin
+                : SystemBarUtils.getQuickQsOffsetHeight(mView.getContext()) -
+                    SystemBarUtils.getStatusBarHeight(mView.getContext());
         mLargeScreenShadeHeaderController.setLargeScreenActive(mUseLargeScreenShadeHeader);
         mAmbientState.setStackTopMargin(topMargin);
         mNotificationsQSContainerController.updateResources();
@@ -2766,7 +2772,7 @@ public final class NotificationPanelViewController extends PanelViewController {
         return Math.max(mQuickQsHeaderHeight * mAmbientState.getExpansionFraction(),
                 mAmbientState.getStackY()
                         // need to adjust for extra margin introduced by large screen shade header
-                        + mAmbientState.getStackTopMargin() * mAmbientState.getExpansionFraction()
+                        + mQsTopMargin * mAmbientState.getExpansionFraction()
                         - mAmbientState.getScrollY());
     }
 
